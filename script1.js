891d62111a2ff8894a872fac58357aefb8a159b5
AAROMAL-ANIL-001 <110799825+AAROMAL-ANIL-001@users.noreply.github.com>
2022-09-27 13:54:43 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -11,7 +11,7 @@ function validateName(){
         nameError.innerHTML ='Name is required';
         return false;
     }
-    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+    if(!name.match(/^[A-Za-z]+\s{1}[A-Za-z]{1,20}$/)){
         nameError.innerHTML ='Write full name';
         return false;
 
